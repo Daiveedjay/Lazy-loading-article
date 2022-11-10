c8b0b2594e2b9ff349e03dbaf836e597c0daef7c
Daiveed <jajadavidjid@gmail.com>
2022-11-10 21:37:51 +0100

    Proper commenting added to js file

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ const imgTargets = document.querySelectorAll("img[data-src]");
 // console.log(imgTargets);
 
 const imgLoad = function (entries, observer) {
-  entries.forEach((entry) => {
-    console.log(entry);
-  });
+  // entries.forEach((entry) => {
+  //   console.log(entry);
+  // });
   // console.log(entries);
   const [entry] = entries;
   if (!entry.isIntersecting) return;
@@ -18,15 +18,18 @@ const imgLoad = function (entries, observer) {
     entry.target.classList.remove("lazy-img");
   });
 
+  // Unboserving elements after loading them
   observer.unobserve(entry.target);
 };
 
+// Intersection observer options object
 const obsOptions = {
   root: null,
   threshold: 0,
-  rootMargin: "-200px",
 };
 
+// Intersection Observer function
 const imgObserver = new IntersectionObserver(imgLoad, obsOptions);
 
+// Loop to obeserve each object
 imgTargets.forEach((img) => imgObserver.observe(img));
